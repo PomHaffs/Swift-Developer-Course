8a4eb475e4c5d3116ae4a106fd0344402f56a6b2
PomHaffs <tomashaffenden@icloud.com>
2016-12-09 11:04:57 +1100

    Added Varibles lesson

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -2,4 +2,85 @@
 
 import UIKit
 
+//The value of VAR can be changed
 var str = "Hello, playground"
+
+str = "Hello, Monkey"
+
+//The value of LET cannot be changed
+let otherStr = "Hello, Tomas"
+//otherStr = "Cannot do this"
+
+                    //String - number, letter etc
+
+let name = "Tomas"
+
+print("Hello " + name)
+
+                    //Integer - whole numbers
+
+let myInt = 33
+
+print(myInt * 5)
+
+print(myInt/2)
+print(myInt%2)
+
+print("myInt has a value of \(myInt)")
+
+let age = 33
+
+print("\(name) is \(age) years old")
+
+                    //Double - decimal
+                    //Float - floating point number
+//ONLY perform calculations on the same types of varibles
+
+let myNum = 23.45
+let myDouble: Double = 342.45
+let myFloat: Float = 23.44444444
+
+//NOPE - age + myNum
+Double(age) + myNum
+
+                    //Boolean
+
+var gameOver = false
+gameOver = true
+
+var gameOverStr = String(gameOver)
+
+var num: Double = 5.76
+var num2: Int = 8
+
+let ans = num * Double(num2)
+
+print("The product of \(num) and \(num2) is \(ans)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
