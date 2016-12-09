0b93a2784b00c42636727523d4abf713d1fc89ed
PomHaffs <tomashaffenden@icloud.com>
2016-12-09 11:46:11 +1100

    Added IfStatement lesson

---
+++ If Statements.playground/Contents.swift
@@ -0,0 +1,70 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let age = 18
+
+if age >= 18 {
+    print("good age!")
+} else {
+    print("Too young for fun")
+}
+
+let name = "kirsten"
+
+if name == "rob" {
+    print("that is my name too!")
+} else {
+    print("that is a shit name")
+}
+
+if age >= 18 && name == "rob" {
+    print("you are exactly who we want")
+} else if name == "kirsten" {
+    print("good age bad name")
+} else {
+    print("jog on cockmister")
+}
+
+
+if name == "rob" || name == "kirsten" {
+    print("there is a dog in the dishwasher")
+} else {
+    print("Washing up needs doing")
+}
+
+let isMale = true
+
+if isMale {
+    print("you are the man")
+}
+
+
+
+let userName = "Tomas"
+let password = "Chicken"
+
+if userName == "Tomas" && password == "Chicken" {
+    print("yep you in!")
+} else if userName != "Tomas" && password != "Chicken" {
+    print("Check your userName and password")
+} else if userName == "Tomas"  {
+    print("Check your password")
+} else {
+    print("Check your userName")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
