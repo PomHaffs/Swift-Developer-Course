80cd05e1f87ab7095b411c40ec4eecac7158c927
PomHaffs <tomashaffenden@icloud.com>
2016-12-09 16:46:06 +1100

    Added While Loops Lesson

---
+++ WhileLoops.playground/Contents.swift
@@ -0,0 +1,59 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var i = 1
+//
+//while i <= 10 {
+//    
+//    print(i)
+//    i += 1
+//}
+
+
+//1. First 20 nums in 7 times table
+//var a = 1
+//
+//while a <= 20 {
+//    
+//    print(a * 7)
+//    a += 1
+//    
+//}
+
+//2. Add one to each value in array
+var i = 0
+
+var array = [7, 23, 98, 1, 0, 763]
+
+array.count
+
+while i < array.count {
+    //array[i] = array[i] + 1
+    
+    array[i] += 1
+    i += 1
+}
+print(array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
