7a616dd784c0a56124e724c2645d4a5faeffa39c
PomHaffs <tomashaffenden@icloud.com>
2016-12-13 14:38:28 +1100

    Added NavBarTask lesson

--- NavBarsTask/NavBarsTask/ViewController.swift
+++ NavBarsTask/NavBarsTask/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func cameraPressed(_ sender: Any) {
+        
+        print("Camera pressed")
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
