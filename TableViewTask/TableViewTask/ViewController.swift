8e1ebd68f7effdaa3b92e9c53229c975c0a845fa
PomHaffs <tomashaffenden@icloud.com>
2016-12-13 16:18:37 +1100

    Added 50 cells task

--- TableViewTask/TableViewTask/ViewController.swift
+++ TableViewTask/TableViewTask/ViewController.swift
@@ -8,7 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var cellContent = 50
+    
+    //inturnal because only need in this class
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 50
+        
+    }
+    //this is what is in each cell
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        while cellContent > 0 {
+            cell.textLabel?.text = String(cellContent)
+            cellContent -= 1
+            return cell
+        }
+        
+        return cell
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
