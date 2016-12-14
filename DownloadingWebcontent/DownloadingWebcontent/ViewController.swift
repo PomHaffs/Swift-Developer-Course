8e782da7e6adceb983b8221b03300f34b8bbef5f
PomHaffs <tomashaffenden@icloud.com>
2016-12-15 10:59:12 +1100

    Added Web URL calls and queuing

--- DownloadingWebcontent/DownloadingWebcontent/ViewController.swift
+++ DownloadingWebcontent/DownloadingWebcontent/ViewController.swift
@@ -16,10 +16,47 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         //define static URL location
-        let url = URL(string: "http://www.stackoverflow.com")!
-        //call load request
-        webView.loadRequest(URLRequest(url: url))
+        //let url = URL(string: "http://www.stackoverflow.com")!
         
+        //call load request to load given page
+        //webView.loadRequest(URLRequest(url: url))
+        
+        //baseURL is optional
+        //webView.loadHTMLString("<h1>Hello Monkey</h1>", baseURL: nil)
+        
+                        //SCRAPING - IMPORTANT look at p.List!!!!
+        if let url = URL(string: "http://stackoverflow.com") {
+            
+            let request = NSMutableURLRequest(url: url)
+        
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                //3 things we get back - data, responce, error
+                data, response, error in
+                
+                if error != nil {
+                    print(error as Any)
+                } else {
+                    if let unwrappedData = data {
+                        //UTF8 is standard encoding
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        
+                        print(dataString as Any)
+                        
+                        DispatchQueue.main.sync(execute: {
+                        
+                            //Update UI would go here AFTER stuff has loaded
+                            
+                        })
+                        
+                    }
+                }
+                
+            }
+            //this is making it RUN or nothing happensd
+            task.resume()
+        }
+        //This is async so will run as data is queued and loading
+        print("printing")
         
     }
 
