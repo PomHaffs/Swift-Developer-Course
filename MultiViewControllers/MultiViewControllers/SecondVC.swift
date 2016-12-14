5e8562e7c345e1bf399e1c9e85ff926a4b50cae6
PomHaffs <tomashaffenden@icloud.com>
2016-12-14 12:08:26 +1100

    Added multiVC task

---
+++ MultiViewControllers/MultiViewControllers/SecondVC.swift
@@ -0,0 +1,37 @@
+//
+//  SecondVC.swift
+//  MultiViewControllers
+//
+//  Created by Tomas-William Haffenden on 14/12/16.
+//  Copyright © 2016 PomHaffs. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("SecondVC loaded correctly")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
