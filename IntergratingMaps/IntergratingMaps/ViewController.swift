9dfd319b55e09b84cb2b6abd40efa6ef4aa59743
PomHaffs <tomashaffenden@icloud.com>
2016-12-21 11:18:01 +1100

    Adde longpress and user annotation

--- IntergratingMaps/IntergratingMaps/ViewController.swift
+++ IntergratingMaps/IntergratingMaps/ViewController.swift
@@ -34,6 +34,33 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         mapView.setRegion(region, animated: true)
         
+        //ADDING annotations to location
+        
+    let annotation = MKPointAnnotation()
+        annotation.title = "Sydney"
+        annotation.subtitle = "this is where i live now!"
+        annotation.coordinate = location
+        
+        mapView.addAnnotation(annotation)
+        
+        //whole screen
+        let longpress = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
+        
+        longpress.minimumPressDuration = 2
+        mapView.addGestureRecognizer(longpress)
+        
+    }
+    
+    //the ':' in 'longpress:' is needed or no information is sent with press
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+        
+        let touchPoint = gestureRecognizer.location(in: self.mapView)
+        let coordinate = mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "Somewhere New"
+        annotation.subtitle = "next place to go!"
+        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
