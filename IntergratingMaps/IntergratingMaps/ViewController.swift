7d16781a9c8e35b8fc027e12f9ac66ac79b0ab0a
PomHaffs <tomashaffenden@icloud.com>
2016-12-21 11:01:43 +1100

    added start location logic and zoom

--- IntergratingMaps/IntergratingMaps/ViewController.swift
+++ IntergratingMaps/IntergratingMaps/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+//ADDED mapkit
+import MapKit
 
-class ViewController: UIViewController {
+//ADDED MKMap....
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Sydney
+        let latitude: CLLocationDegrees = -33.8756271
+        let longitude: CLLocationDegrees = 151.2160727
+        //This is zoom
+        let latDelta: CLLocationDegrees = 5.0
+        let longDelta: CLLocationDegrees = 5.0
+        //overall zoom level, combo of latDelta and longDelta
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
